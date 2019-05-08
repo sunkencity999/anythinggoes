1e72362fe3f6a8457935c4f83219fa2c5d1a3f9d
aprwebmaster <cbradford@apr.com>
2019-05-08 15:23:16 -0700

    added testing and code to implement the Post resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  
 
-  get 'home/about'
-
-  get 'home/contact'
+  resources :posts
+  
+  get 'about' => 'home#about'
+  get 'contact' => 'home#contact'
   
   root 'welcome#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end

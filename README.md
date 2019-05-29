df67ebb7468a7739724088344dc8f5491d8e91c2
aprwebmaster <cbradford@apr.com>
2019-05-29 11:39:32 -0700

    added comments to user, created comment tests, wrote code to pass tests unrelated to roles

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# README
+# Anything Goes School of Martial Arts!
 
 This README would normally document whatever steps are necessary to get the
 application up and running.
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,4 +2,111 @@ require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
 
+let(:my_user) { User.create!( email: "user@anythinggoes.com", password: "helloworld") }
+let(:other_user) { User.create!( email: "otheruser@anythinggoes.com", password: "helloworld", role: :member) }
+let(:my_post) { Post.create!(title: "Test Title", body: "Test Body", user: my_user) }
+let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user) }
+
+context "guest" do
+     describe "POST create" do
+       it "redirects the user to the sign in view" do
+         post :create, params: { post_id: my_post.id, comment: { body: "This is test body content." } }
+         expect(response).to redirect_to(new_user_session_path)
+       end
+     end
+ 
+     describe "DELETE destroy" do
+       it "redirects the user to the sign in view" do
+         delete :destroy, params: { post_id: my_post.id, id: my_comment.id }
+         expect(response).to redirect_to(new_user_session_path)
+       end
+     end
+   end
+ 
+   context "member user doing CRUD on a comment they don't own" do
+     before do
+       sign_in other_user
+     end
+ 
+     describe "POST create" do
+       it "increases the number of comments by 1" do
+         expect{ post :create, params: { post_id: my_post.id, comment: {body:"This is test body content."} } }.to change(Comment,:count).by(1)
+       end
+ 
+       it "redirects to the post show view" do
+         post :create, params: { post_id: my_post.id, comment: {body:"This is test body content."} }
+         expect(response).to redirect_to [my_post]
+       end
+     end
+ 
+     describe "DELETE destroy" do
+       it "redirects the user to the posts show view" do
+         delete :destroy, params: { post_id: my_post.id, id: my_comment.id }
+         expect(response).to redirect_to([my_post])
+       end
+     end
+   end
+ 
+ 
+   context "member user doing CRUD on a comment they own" do
+     before do
+       sign_in my_user
+     end
+ 
+     describe "POST create" do
+       it "increases the number of comments by 1" do
+         expect{ post :create, params: { post_id: my_post.id, comment: {body:"This is test body content."} } }.to change(Comment,:count).by(1)
+       end
+ 
+       it "redirects to the post show view" do
+         post :create, params: { post_id: my_post.id, comment: {body:"This is test body content."} }
+         expect(response).to redirect_to [my_post]
+       end
+     end
+ 
+     describe "DELETE destroy" do
+       it "deletes the comment" do
+         delete :destroy, params: { post_id: my_post.id, id: my_comment.id }
+         count = Comment.where({id: my_comment.id}).count
+         expect(count).to eq 0
+       end
+ 
+       it "redirects to the post show view" do
+         delete :destroy, params: { post_id: my_post.id, id: my_comment.id }
+         expect(response).to redirect_to [my_post]
+       end
+     end
+   end
+ 
+   context "admin user doing CRUD on a comment they don't own" do
+     before do
+       other_user.admin!
+       sign_in other_user
+     end
+ 
+     describe "POST create" do
+       it "increases the number of comments by 1" do
+         expect{ post :create, params: { post_id: my_post.id, comment: {body:"This is test body content."} } }.to change(Comment,:count).by(1)
+       end
+ 
+       it "redirects to the post show view" do
+         post :create, params: { post_id: my_post.id, comment: {body:"This is test body content."} }
+         expect(response).to redirect_to [my_post]
+       end
+     end
+ 
+     describe "DELETE destroy" do
+       it "deletes the comment" do
+         delete :destroy, params: { post_id: my_post.id, id: my_comment.id }
+         count = Comment.where({id: my_comment.id}).count
+         expect(count).to eq 0
+       end
+ 
+       it "redirects to the post show view" do
+         delete :destroy, params: { post_id: my_post.id, id: my_comment.id }
+         expect(response).to redirect_to [my_post]
+       end
+     end
+   end
 end
+

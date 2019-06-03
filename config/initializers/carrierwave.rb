1a1ea98df21192580b1e3d5d4a84e1c823977c19
aprwebmaster <cbradford@apr.com>
2019-06-03 13:32:14 -0700

    more bug fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        
   config.fog_credentials = {
     :provider =>              'AWS',       
-    :aws_access_key_id =>     ENV["S3_KEY"],    
-    :aws_secret_access_key => ENV["S3_SECRET_KEY"]
+    :aws_access_key_id =>     ENV["AWS_ACCESS_KEY_ID"],    
+    :aws_secret_access_key => ENV["AWS_SECRET_KEY"]
    # use_iam_profile:       true,                         # optional, defaults to false
    # region:                'us-east-1',                  # optional, defaults to 'us-east-1'
    # host:                  's3.example.com',             # optional, defaults to nil

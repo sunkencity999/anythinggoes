99ba7d9bfb1f2c8815612026d747b5d6d8605101
aprwebmaster <cbradford@apr.com>
2019-07-10 14:41:54 -0700

    Corrected the tests to account for removed pages

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -8,19 +8,4 @@ RSpec.describe HomeController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET #about" do
-    it "returns http success" do
-      get :about
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #contact" do
-    it "returns http success" do
-      get :contact
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
